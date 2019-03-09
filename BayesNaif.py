Fix bug covariance

BayesNaif.py
@@ -10,7 +10,6 @@ je vais avoir besoin de tester les méthodes test, predict et test de votre code
 import numpy as np
 
 class BayesNaif:
-    def __init__(self):
         
     def train(self, train, train_labels):
         self.cnt_classes_occ = np.bincount(train_labels)
@@ -20,7 +19,7 @@ class BayesNaif:
         self._shared_cov_mat = self._shared_naive_cov_mats()
 
     def predict(self, exemple, label):
-        h = ((-0.5) * np.sum(np.power(exemple[None, :] - self._means, 2) / 
+        h = ((-1/2) * np.sum(np.power(exemple[None, :] - self._means, 2) / 
             np.diagonal(self._shared_cov_mat)[None, :], axis=1) +
             np.log(self._priors))
         pred = np.argmax(h)
@@ -65,7 +64,7 @@ class BayesNaif:
                     mat_i.append(data)
 
             mat_i = np.asarray(mat_i)
-            cov_mat_i.append(np.cov(mat_i.T, bias=False))
+            cov_mat_i.append(np.cov(mat_i.T, bias=True))
 
         return np.asarray(cov_mat_i)
 

entrainer_tester.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     # Initializer vos paramètres
 
-    train, train_labels, test, test_labels = load_datasets.load_iris_dataset(0.3)
+    train, train_labels, test, test_labels = load_datasets.load_iris_dataset(0.8)
 
     # Initializer/instanciez vos classifieurs avec leurs paramètres
 
@@ -39,19 +39,7 @@ if __name__ == "__main__":
 
     b.test(test, test_labels)
     rr = clf.score(test, test_labels)
-    # true = []
-    # tt = []
-    # count = 0
-    # b.predict(test[24], test_labels[24])
-    # for x, y in zip(test, test_labels):
-    #     true.append(clf.predict(np.asarray([x])))
-    #     tt.append(b.predict(x, y))
-    #     if b.predict(x, y)!= clf.predict(np.asarray([x]))[0]:
-    #         print(x, "ERROOOOOOORRR")
-    #     print(b.predict(x, y), clf.predict(np.asarray([x]))[0])
-    #     count = count +1
-    # print(tt)
-    # print(true)
+
 
 
     # Charger/lire les datasets